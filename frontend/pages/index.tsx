18c32ac6eeea72b8a0b672b0a2eb9a72fb202d04
Elias <elias.sabja@uc.cl>
2022-05-25 19:10:05 -0400

    Color update

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,14 @@
-import type {ReactElement} from 'react';
+import type { ReactElement } from 'react';
 import { useRouter } from 'next/router';
 import CatalogLayout from '../components/catalog/catalogLayout';
-
+import { Drawer } from '@mui/material';
 const Index: ReactElement = () => {
 
-  return(
-    <CatalogLayout />  
-  ); 
+  return (
+    <Drawer>
+      <CatalogLayout />
+    </Drawer>
+  );
 };
 
 export default Index;
